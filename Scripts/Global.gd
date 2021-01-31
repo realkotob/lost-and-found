a71f7d103c28a46b16628737a8730ae835357d89
asheraryam <asheraryam@gmail.com>
2021-01-31 19:05:14 +0300

    Add win condition

--- Scripts/Global.gd
+++ Scripts/Global.gd
@@ -12,6 +12,8 @@ signal you_lose
 signal update_player_pos(global_x,global_y)
 
 
+var game_stopped = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
--- Scripts/Helicopter.gd
+++ Scripts/Helicopter.gd
@@ -20,6 +20,16 @@ var check_height = 0
 var des_height = 0
 func _process(delta):
 	var move_vec = Vector3(0,0,0)
+	if force_move_to_ground_player:
+		move_vec = Global.ground_player.global_transform.origin - global_transform.origin
+		if move_vec.length_squared() > 1:
+			move_vec = move_vec.normalized() * delta * player_speed
+			global_transform.origin += move_vec
+		return
+	
+	if Global.game_stopped:
+		return
+		
 	if Input.is_action_pressed("p%s_down" %str(player_number)):
 		move_vec += horizontalify(cam.global_transform.basis.z)
 	if Input.is_action_pressed("p%s_up" %str(player_number)):
@@ -57,3 +67,12 @@ func _process(delta):
 		des_height = y_pos + 15
 func get_compass_rotation():
 	return $CameraHelper.rotation.y
+
+var force_move_to_ground_player = false
+func _on_Area_body_entered(body):
+	print("Body entered")
+	if "player_number" in body and body.player_number == 1:
+		print("You win")
+		Global.game_stopped = true
+		Global.emit_signal("you_win")
+		force_move_to_ground_player = true
