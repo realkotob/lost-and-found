f55c9f4653b20f3b8c905eb0e7ab719e895620f3
asheraryam <asheraryam@gmail.com>
2021-01-31 17:23:14 +0300

    Add code for new player. Restrict players to game area.

--- Scripts/GroundPlayer.gd
+++ Scripts/GroundPlayer.gd
@@ -1,17 +1,12 @@
 extends Spatial
 
-
-var player_number = 2
-var camera_path
-
-onready var cam = $CameraParent/Camera
-
 export(float) var player_speed = 40
 
+var player_number = 1
+onready var cam = $CameraHelper/Camera
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-
 
 func horizontalify(vec : Vector3):
 	return (vec - Vector3.UP*vec.dot( Vector3.UP)).normalized()
@@ -26,16 +21,12 @@ func _process(delta):
 		move_vec += horizontalify(cam.global_transform.basis.x)
 	if Input.is_action_pressed("p%s_left" %str(player_number)):
 		move_vec -= horizontalify(cam.global_transform.basis.x)
-	
+		
 	var next_pos = global_transform.origin + move_vec * delta * player_speed
 	if next_pos.z < 384 and next_pos.x < 384 and next_pos.z > 0 and next_pos.x > 0:
 		global_transform.origin = next_pos
 	
-	if Input.is_action_just_pressed("toggle_map"):
-		Global.emit_signal("toggle_game_map")
-	Global.emit_signal("update_player_pos",global_transform.origin.x, global_transform.origin.z)
-	
 	var rayobj = $RayCast.get_collider()
 	if rayobj:
 		var y_pos = $RayCast.get_collision_point().y
-		global_transform.origin.y = y_pos + 15
+		global_transform.origin.y = y_pos + 0.01
